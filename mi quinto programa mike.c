6b3e663faadd3ecf372b0adee9a04b2adf82ac3d
MiguelDeLunaPonce2007 <64348087+MiguelDeLunaPonce2007@users.noreply.github.com>
2020-04-29 20:16:39 -0500

    Add files via upload

---
+++ mi quinto programa mike.c
@@ -0,0 +1,11 @@
+//Prime Video
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+    printf("What is Prime Video?:\n");
+    printf("The\t App\t called\t Prime\t Video\t is\t a\t app\t that\t you\t can\t see\t movies\t,is\t increible\t ");
+
+    return 0;
+}
